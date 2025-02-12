4ab49cfd6d04478c8f98e3f37e691151afc4efb2
yeyo671 <leotran671@gmail.com>
2025-02-12 14:57:38 +0100

    Initialized the chessboard with 'O'

--- src/nQueens.ts
+++ src/nQueens.ts
@@ -2,5 +2,7 @@ export function solveNQueens(n: number): string[][][] {
     if (n === 0) return [];
     if (n === 1) return [[["#"]]];
     if (n === 2 || n === 3) return [];
+    
+    const board: string[][] = Array.from({ length: n }, () => Array(n).fill('O'));
     return [];
 }
