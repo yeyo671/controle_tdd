64b00ef2729e63e3bf0a508b36ca29b4001c57a8
yeyo671 <leotran671@gmail.com>
2025-02-12 14:56:34 +0100

    Add test and implementation for n = 3 (no solution)

--- test/nQueens.test.ts
+++ test/nQueens.test.ts
@@ -4,12 +4,14 @@ test('solveNQueens should return an empty array for n = 0', () => {
   expect(solveNQueens(0)).toEqual([]);
 });
 
-test('solveNQueens should return the correct solution for n = 1', () => {
-  expect(solveNQueens(1)).toEqual([
-    [["#"]]
-  ]);
+test('solveNQueens should return a single solution for n = 1', () => {
+  expect(solveNQueens(1)).toEqual([[["#"]]]);
 });
 
 test('solveNQueens should return an empty array for n = 2', () => {
   expect(solveNQueens(2)).toEqual([]);
+});
+
+test('solveNQueens should return an empty array for n = 3', () => {
+  expect(solveNQueens(3)).toEqual([]);
 });
